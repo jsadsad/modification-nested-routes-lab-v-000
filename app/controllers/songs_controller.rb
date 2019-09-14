0cee88b1774a484507228e9ea7653cd4787a1b8f
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-09-14 02:29:44 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
 
   def new
     if params[:artist_id] && !Artist.find_by(params[:artist_id])
-      redirect_to artist_path, alert: "Artist not found."
+      redirect_to artists_path, alert: "Artist not found."
     else
       @song = Song.new(artist_id: params[:artist_id])
     end
